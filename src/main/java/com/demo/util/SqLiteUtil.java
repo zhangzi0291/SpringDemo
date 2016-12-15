8bcee2249efb408102078b643d2579d63e796a96
North_ZX <zhangszi0291>
2016-12-15 14:32:13 +0800

    sqLite工具类

---
+++ src/main/java/com/demo/util/SqLiteUtil.java
@@ -0,0 +1,140 @@
+/**
+ * CREATE TABLE test_table(
+ *		   column1 INTEGER  PRIMARY KEY,
+ *		   column2 VARCHAR(255),
+ *		   column3 DATETIME
+ *		);
+ */
+package com.demo.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+
+public class SqLiteUtil {
+
+	private static Connection c;
+	
+	/**
+	 * 
+	  * 打开数据库连接
+	  *@return 
+	  *@date 2016年12月15日 上午9:37:46
+	  *@author zxn
+	 */
+	private static Connection getConnection(){
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:E:\\test\\pokemon\\pokemon.db");
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	   return c;
+	}
+	
+	/**
+	 * 
+	  * 关闭数据库连接
+	  *@date 2016年12月15日 上午9:38:15
+	  *@author zxn
+	 */
+	private static  void closeConnection(){
+		try {
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 
+	  * 执行查询返回map结果
+	  *@param sql 要执行的sql语句，预处理型
+	  *@param param 预处理中？的值
+	  *@return 
+	  *@date 2016年12月15日 上午9:38:35
+	  *@author zxn
+	 */
+	public static List<Map<String, String>>  getRowValue(String sql,List<String> param){
+		getConnection();
+		List<Map<String, String>> list=new LinkedList<>();
+		try {
+			PreparedStatement pst=c.prepareStatement(sql);
+			for(int i=0;i<param.size();i++){
+				pst.setString(i+1, param.get(i));
+			}
+			ResultSet rs=pst.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+//		getColumnCount(); 返回 ResultSet 中的列数。 
+//		getColumnName(int); 返回列序号为 int 的列名。 
+			while(rs.next()){
+				Map<String, String> map=new LinkedHashMap<>();
+				for(int i=0;i<rsmd.getColumnCount();i++){
+					map.put(rsmd.getColumnName(i+1), rs.getString(i+1));
+				}
+				list.add(map);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			closeConnection();
+		}
+		return list;
+	}
+	/**
+	 * 
+	  * 执行新增或修改语句
+	  *@param sql 要执行的sql语句，预处理型
+	  *@param param 预处理中？的值
+	  *@return 
+	  *@date 2016年12月15日 上午9:40:03
+	  *@author zxn
+	 */
+	public static Integer  updateValue(String sql,List<String> param){
+		getConnection();
+		Integer num=0;
+		try {
+			PreparedStatement pst=c.prepareStatement(sql);
+			for(int i=0;i<param.size();i++){
+				pst.setString(i+1, param.get(i));
+			}
+			num = pst.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			closeConnection();
+		}
+		return num;
+	}
+	
+	/**
+	 * 
+	  * 执行建表语句
+	  *@param sql 
+	  *@date 2016年12月15日 上午9:40:48
+	  *@author zxn
+	 */
+	public static Integer createTable(String sql){
+		getConnection();
+		Integer num=0;
+		try {
+			Statement st=c.createStatement();
+			num=st.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			closeConnection();
+		}
+		return num;
+	}
+}
